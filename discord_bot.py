163b5ba97294b64b9139d4fa891802554bb5aba7
Noah Nicolas <noahesteve.nicolas@gmail.com>
2025-05-12 00:47:39 +0200

    feat: messages limit

--- discord_bot.py
+++ discord_bot.py
@@ -11,18 +11,31 @@ load_dotenv()
 RESULTS_BASE_URL = os.getenv("RESULTS_BASE_URL")
 
 @bot.command(description="Bilan cumulatif des x dernières semaines")
-async def bilan(ctx, weeks: int, skip_weeks: int = 0):
+async def bilan(ctx, weeks: int, skip_weeks: int = 0, limit: int = 10):
+    characters_limit = 70 # Clash Royale limit
     url = f"{RESULTS_BASE_URL}?weeks={weeks}?skip_weeks={skip_weeks}"
 
     try:
         response = requests.get(url)
         if response.status_code == 200:
-            data = response.json()["results"]
+            data = response.json()["results"][:limit]  # Apply the limit
 
-            message = ""
-            for i in range(len(data)):
-                message += f"{i+1} - {data[i]}\n"
-            await ctx.respond(message)
+            messages = []
+            current_message = ""
+
+            for i, line in enumerate(data, start=1):
+                next_line = f"{i} - {line}\n"
+                if len(current_message) + len(next_line) > characters_limit:
+                    messages.append(current_message)
+                    current_message = next_line
+                else:
+                    current_message += next_line
+
+            if current_message:
+                messages.append(current_message)
+
+            for msg in messages:
+                await ctx.respond(msg)
 
         else:
             # Handle cases where the request fails
