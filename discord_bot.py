972cedff50d7526483b9475126bb43ab90cd875a
Noah Nicolas <noahesteve.nicolas@gmail.com>
2025-05-12 01:02:47 +0200

    fix: updated char count

--- discord_bot.py
+++ discord_bot.py
@@ -12,7 +12,7 @@ RESULTS_BASE_URL = os.getenv("RESULTS_BASE_URL")
 
 @bot.command(description="Bilan cumulatif des x dernières semaines")
 async def bilan(ctx, weeks: int, skip_weeks: int = 0, limit: int = 10, ranking: bool = True):
-    characters_limit = 70 # Clash Royale limit
+    characters_limit = 256 # Clash Royale limit
     url = f"{RESULTS_BASE_URL}?weeks={weeks}&skip_weeks={skip_weeks}&ranking={ranking}"
 
     try:
